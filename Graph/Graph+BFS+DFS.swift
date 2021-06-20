0ce4e5414f7049e146f2e5140ca85e7bf44ef782
panda <iospandaman@gmail.com>
2021-06-21 03:52:41 +0900

    2021.06.21 Add DFS into graph.swift and update Readme.md

--- Graph/Graph+BFS+DFS.swift
+++ Graph/Graph+BFS+DFS.swift
@@ -68,3 +68,36 @@ extension Graph where Element: Hashable {
     return visited
   }
 }
+
+// DFS
+extension Graph where Element: Hashable {
+  public func depthFirstSearch(from source: Vertex<Element>) -> [Vertex<Element>] {
+    var stack: Stack<Vertex<Element>> = []
+    var pushed: Set<Vertex<Element>> = []
+    var visited: [Vertex<Element>] = []
+    
+    stack.push(source)
+    pushed.insert(source)
+    visited.append(source)
+    
+    outer: while let vertex = stack.peek() {
+      let neighbors = edges(from: vertex)
+      guard !neighbors.isEmpty else {
+        stack.pop()
+        continue
+      }
+      for edge in neighbors {
+        if !pushed.contains(edge.destination) {
+          stack.push(edge.destination)
+          pushed.insert(edge.destination)
+          visited.append(edge.destination)
+          continue outer
+        }
+      }
+      stack.pop()
+    }
+    
+    return visited
+  }
+}
+
--- README.md
+++ README.md
@@ -34,14 +34,13 @@
 ## **TIL**
 
 - ### 2021.06.18
-  
   - Quick Sort의 개념과 여러 피봇 선택 전략 정리.
   
 - ### 2021.06.19
-
   - Graph의 개념과 Adjacency list를 이용한 그래프 구현을 정리.
 
 - ### 2021.06.20
-
   - Adjacency Metrix를 이용한 Graph 구현과 BFS 알고리즘 구현을 정리.
-
+  
+- ### 2021.06.21
+  - Graph를 순회(traverse)하거나 탐색(search)하는 알고리즘인 Depth-Frist-Search(DFS)의 개념을 익히고 구현.
