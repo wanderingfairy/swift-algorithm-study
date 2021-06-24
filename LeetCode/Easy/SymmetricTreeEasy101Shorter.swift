ff426f5b2e6cc160f2489509924ce4d71e93bec3
wanderingfairy <56634480+wanderingfairy@users.noreply.github.com>
2021-06-24 16:10:14 +0900

    Update SymmetricTreeEasy101Shorter.swift

--- LeetCode/Easy/SymmetricTreeEasy101Shorter.swift
+++ LeetCode/Easy/SymmetricTreeEasy101Shorter.swift
@@ -1,17 +1,17 @@
-import Foundation
+//import Foundation
 
-public class TreeNode {
-  public var val: Int
-  public var left: TreeNode?
-  public var right: TreeNode?
-  public init() { self.val = 0; self.left = nil; self.right = nil; }
-  public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
-  public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
-    self.val = val
-    self.left = left
-    self.right = right
-  }
-}
+//public class TreeNode {
+//  public var val: Int
+//  public var left: TreeNode?
+//  public var right: TreeNode?
+//  public init() { self.val = 0; self.left = nil; self.right = nil; }
+//  public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+//  public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+//    self.val = val
+//    self.left = left
+//    self.right = right
+//  }
+//}
 
 class Solution {
   
